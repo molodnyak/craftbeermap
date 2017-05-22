ef213130f7f77d1686286e4206f94ed842001f0a
molodnyak <molodnyak.evgeny@gmail.com>
2017-05-22 22:13:23 +0200

    Update fb.R

--- fb.R
+++ fb.R
@@ -14,3 +14,65 @@ ratingdata <- fromJSON("https://www.googleapis.com/fusiontables/v1/query?sql=SEL
 rd <- data.frame(ratingdata$rows, stringsAsFactors = F)
 colnames(rd) <- ratingdata$columns
 
+#----------------------------------------------
+#Facebook.ID
+x <- strsplit(rd$FB, '/')
+rd$FBID <- unlist(lapply(x, function(x) x[4]))
+
+#Facebook.API
+##ToGEtToken Open https://developers.facebook.com/tools/explorer
+tokenfb <- "EAACEdEose0cBALhttKTY1gZA5JSLdm9V07Kg0kzMeWQJ5MIDXCCLRz8B9MZBofZA00Os1t4q6EbUwCzRxLWf7Rk0ohc18BCbpZBFAnHWwpEI8Iyfi6jj1fXfTsLI6ThY5GC3td4hk0JObp7u9GokRg4yv9oaZCMzdlCn6OaLjMWjtsQ2HLxvHeOZAhCvZAEIvwZD"
+baseurlfb <- "https://graph.facebook.com/v2.8/"
+
+
+#HTTP error 400 Control
+rd[,"FBname"] <- NA
+rd[,"FBcity"] <- NA
+rd[,"FBstreet"] <- NA
+rd[,"FBlatitude"] <- NA
+rd[,"FBlongitude"] <- NA
+rd[,"FBbirthday"] <- NA
+rd[,"FBphone"] <- NA
+rd[,"FBhours"] <- NA
+rd[,"FBfan_count"] <- NA
+rd[,"FBcheckins"] <- NA
+rd[,"FBrating_count"] <- NA
+
+#LOOP=fields
+for (i in 1:nrow(rd)){
+  tryCatch({
+    fb <- fromJSON(paste0(baseurlfb, rd$FBID[i], "?fields=", 
+                          paste("name", "location", "birthday", "phone", "hours", "fan_count", "checkins", "rating_count", sep = "%2C"), 
+                          "&access_token=", tokenfb))
+    if(fb[[1]] == "") {stop("ERROR: Facebook Page is unavailable ! - ", rd$FBID[i])} 
+    else { 
+      if(fb[[1]] != "") {
+        
+        rd$FBname[i] <- fb$name
+        rd$FBcity[i] <- ifelse(is.null(fb$location$city) == T, "", fb$location$city)
+        rd$FBstreet[i] <- ifelse(is.null(fb$location$street) == T, "", fb$location$street)
+        rd$FBlatitude[i] <- ifelse(is.null(fb$location$latitude) == T, "", fb$location$latitude)
+        rd$FBlongitude[i] <- ifelse(is.null(fb$location$longitude) == T, "", fb$location$longitude)
+        rd$FBbirthday[i] <- ifelse(is.null(fb$birthday) == T, "", fb$birthday)
+        rd$FBphone[i] <- ifelse(is.null(fb$phone) == T, "", fb$phone)
+        rd$FBhours[i] <- ifelse(is.null(fb$hours$mon_1_close) == T, "", local({
+          monday <- ifelse(as.numeric(as.difftime(fb$hours$mon_1_close, format = "%H:%M")) < 8, as.numeric(as.difftime(fb$hours$mon_1_close, format = "%H:%M")) + 24 - as.numeric(as.difftime(fb$hours$mon_1_open, format = "%H:%M")), as.numeric(as.difftime(fb$hours$mon_1_close, format = "%H:%M")) - as.numeric(as.difftime(fb$hours$mon_1_open, format = "%H:%M")))
+          tuesday <- ifelse(as.numeric(as.difftime(fb$hours$tue_1_close, format = "%H:%M")) < 8, as.numeric(as.difftime(fb$hours$tue_1_close, format = "%H:%M")) + 24 - as.numeric(as.difftime(fb$hours$tue_1_open, format = "%H:%M")), as.numeric(as.difftime(fb$hours$tue_1_close, format = "%H:%M")) - as.numeric(as.difftime(fb$hours$tue_1_open, format = "%H:%M")))
+          wednesday <- ifelse(as.numeric(as.difftime(fb$hours$wed_1_close, format = "%H:%M")) < 8, as.numeric(as.difftime(fb$hours$wed_1_close, format = "%H:%M")) + 24 - as.numeric(as.difftime(fb$hours$wed_1_open, format = "%H:%M")), as.numeric(as.difftime(fb$hours$wed_1_close, format = "%H:%M")) - as.numeric(as.difftime(fb$hours$wed_1_open, format = "%H:%M")))
+          thursday <- ifelse(as.numeric(as.difftime(fb$hours$thu_1_close, format = "%H:%M")) < 8, as.numeric(as.difftime(fb$hours$thu_1_close, format = "%H:%M")) + 24 - as.numeric(as.difftime(fb$hours$thu_1_open, format = "%H:%M")), as.numeric(as.difftime(fb$hours$thu_1_close, format = "%H:%M")) - as.numeric(as.difftime(fb$hours$thu_1_open, format = "%H:%M")))
+          friday <- ifelse(as.numeric(as.difftime(fb$hours$fri_1_close, format = "%H:%M")) < 8, as.numeric(as.difftime(fb$hours$fri_1_close, format = "%H:%M")) + 24 - as.numeric(as.difftime(fb$hours$fri_1_open, format = "%H:%M")), as.numeric(as.difftime(fb$hours$fri_1_close, format = "%H:%M")) - as.numeric(as.difftime(fb$hours$fri_1_open, format = "%H:%M")))
+          saturday <- ifelse(as.numeric(as.difftime(fb$hours$sat_1_close, format = "%H:%M")) < 8, as.numeric(as.difftime(fb$hours$sat_1_close, format = "%H:%M")) + 24 - as.numeric(as.difftime(fb$hours$sat_1_open, format = "%H:%M")), as.numeric(as.difftime(fb$hours$sat_1_close, format = "%H:%M")) - as.numeric(as.difftime(fb$hours$sat_1_open, format = "%H:%M")))
+          sunday <- ifelse(as.numeric(as.difftime(fb$hours$sun_1_close, format = "%H:%M")) < 8, as.numeric(as.difftime(fb$hours$sun_1_close, format = "%H:%M")) + 24 - as.numeric(as.difftime(fb$hours$sun_1_open, format = "%H:%M")), as.numeric(as.difftime(fb$hours$sun_1_close, format = "%H:%M")) - as.numeric(as.difftime(fb$hours$sun_1_open, format = "%H:%M")))
+          as.numeric(monday+tuesday+wednesday+thursday+friday+saturday+sunday)
+        }, envir = new.env()))
+        rd$FBfan_count[i] <- fb$fan_count
+        rd$FBcheckins[i] <- fb$checkins
+        rd$FBrating_count[i] <- fb$rating_count
+
+      }
+    }
+    message("Scraping Facebook page ", paste(rd$FBID[i], i, "from", nrow(rd), sep = " "))
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+}
+
+rd$FBhours <- as.numeric(rd$FBhours)
