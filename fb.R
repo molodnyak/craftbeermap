3f15696a4cbc8f54298935ed8e5389fbdedc9ed2
molodnyak <molodnyak.evgeny@gmail.com>
2017-06-12 04:36:40 +0200

    Create fb.R

--- fb.R
+++ fb.R
@@ -1,16 +1,6 @@
 
 library(jsonlite)
 library(curl)
-
-#GOOGLE.Fusion.Tables.API
-
-#Juin17
-ratingdata <- fromJSON("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+Type,Name,Address,Time,Phone,VK,FB,INST,FRSQ,UNTAPPD,OpenDate,Card,Tap,Seat,Street,WC,Bartender,Liquor,Waiter,WiFi,Cuisine+FROM+1ZXX8vWvi8FQI6TSPtlNHX7gvcUefTlIzBdcspfaY++WHERE+Type+IN+('Бар','Магазин-бар')+and+Address+not+equal+to+''+and+Response+>=+5+and+Response+<=+10+order+by+Name+asc&key=AIzaSyD9OWz96j4E0XgWWFwxL7T-G_ilz0o11Vk")
-
-#DataFrame
-rd <- data.frame(ratingdata$rows, stringsAsFactors = F)
-colnames(rd) <- ratingdata$columns
-
 #----------------------------------------------
 #Facebook.ID
 x <- strsplit(rd$FB, '/')
