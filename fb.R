55bca772487b3488646fcf29fa1feaf5a914f400
molodnyak <molodnyak.evgeny@gmail.com>
2017-06-12 04:35:09 +0200

    Create fb.R

--- fb.R
+++ fb.R
@@ -4,7 +4,7 @@ library(curl)
 
 #GOOGLE.Fusion.Tables.API
 
-#May17
+#Juin17
 ratingdata <- fromJSON("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+Type,Name,Address,Time,Phone,VK,FB,INST,FRSQ,UNTAPPD,OpenDate,Card,Tap,Seat,Street,WC,Bartender,Liquor,Waiter,WiFi,Cuisine+FROM+1ZXX8vWvi8FQI6TSPtlNHX7gvcUefTlIzBdcspfaY++WHERE+Type+IN+('Бар','Магазин-бар')+and+Address+not+equal+to+''+and+Response+>=+5+and+Response+<=+10+order+by+Name+asc&key=AIzaSyD9OWz96j4E0XgWWFwxL7T-G_ilz0o11Vk")
 
 #DataFrame
@@ -115,7 +115,7 @@ for (i in 1:nrow(rd)){
     else { 
       if(length(posts) == "2") {
         
-        rd$FBInter[i] <- round(1-insights$data$values[[1]][[1]][3,]$RU/sum(na.omit(unlist(insights$data$values[[1]]$value[3,]))), digits=3)
+        rd$FBInter[i] <- round(1-insights$data$values[[1]][[1]][2,]$RU/sum(na.omit(unlist(insights$data$values[[1]]$value[2,]))), digits=3)
 
       }
     }
